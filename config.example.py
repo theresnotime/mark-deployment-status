01ceb7194fa7c74684fac39fa05e72d9076daf00
TheresNoTime <sam@theresnotime.co.uk>
2025-04-09 16:34:51 +0100

    emdash worst dash

--- config.example.py
+++ config.example.py
@@ -2,5 +2,5 @@ TNT_BOT_PASS = ""
 DEPLOYMENT_PAGE = "Deployments"
 EDIT_SUMMARY = "[[User:TNTBot#Updating_backport_window_deployment_statuses|Automated task]]: Updating deployment statuses"
 USER_AGENT = (
-    "TNTBot (https://meta.wikimedia.org/wiki/User:TNTBot) — mark-deployment-status"
+    "TNTBot (https://meta.wikimedia.org/wiki/User:TNTBot) - mark-deployment-status"
 )
